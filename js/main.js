c6fd4fcc954a9983932de77d92f0759ad2fa6acd
Emerson Ramirez <emerson.ramirez.s@gmail.com>
2023-01-09 23:41:57 -0300

    Previo a modals

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const clickMap = (opcion) => {
+    switch (opcion) {
+        case 0: console.log('se selecciono el barril 1'); break;
+        case 1: alert('cod-barra');
+        case 2: alert('trackeo');
+        case 3: alert('tiempo');
+        case 4: alert('graficos');
+        case 5: alert('barril2');
+        case 6: alert('brewery');
+        case 7: alert('beershop');       
+    }
+}
